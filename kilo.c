be09e32429b577825b8e158e498f30e4f7866276
BucBharat <bharat.buchupalle1234@gmail.com>
2026-09-04 17:12:26 +0530

    update terminal flags to disable input processing and signals in raw mode

--- kilo.c
+++ kilo.c
@@ -10,7 +10,8 @@ void enableRawMode() {
   atexit(disableRawMode);
   struct termios raw;
   raw = orig_termios;
-  raw.c_lflag &= ~(ECHO | ICANON);
+  raw.c_iflag &= ~(ICRNL | IXON);
+  raw.c_lflag &= ~(IEXTEN | ECHO | ICANON | ISIG);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 int main() {
